27c1473d11357d3415c92fb28e4a3581c2ef7e2c
tarepan <tarepan5884@gmail.com>
2023-03-16 05:21:54 +0000

    Refactor core

--- ddsp/core.py
+++ ddsp/core.py
@@ -6,17 +6,16 @@ import math
 import numpy as np
 
 
-def logb(x, base=2.0, safe=False):
-  """Logarithm with base as an argument."""
-  if safe:
-    return safe_divide(safe_log(x), safe_log(base))
-  else:
+#### unit_to_hz2 ######################################################################################
+def _logb(x):
+    """Logarithm with base as an argument."""
+    base=2.0
     return torch.log(torch.tensor(x)) / torch.log(torch.tensor(base))    
 
 
-def hz_to_midi(frequencies):
+def _hz_to_midi(frequencies: float):
     """TF-compatible hz_to_midi function."""
-    notes = 12.0 * (logb(frequencies, 2.0) - logb(torch.tensor(440.0).float(), 2.0)) + 69.0
+    notes = 12.0 * (_logb(frequencies) - _logb(torch.tensor(440.0).float())) + 69.0
     # Map 0 Hz to MIDI 0 (Replace -inf MIDI with 0.)
     notes = F.relu(notes)
     #notes = notes.double()
@@ -25,22 +24,78 @@ def hz_to_midi(frequencies):
     return notes.float()
 
 
-def midi_to_hz(notes):
+def _midi_to_hz(notes):
   """TF-compatible midi_to_hz function."""
   return 440.0 * (2.0**((notes - 69.0) / 12.0))
 
 
-def unit_to_hz2(unit,
-               hz_min,
-               hz_max,
-               clip: bool = False) :
-    """Map unit interval [0, 1] to [hz_min, hz_max], scaling logarithmically."""
-    midi_max = hz_to_midi(hz_max)
-    midi_min = hz_to_midi(hz_min)
-    return midi_to_hz((midi_max - midi_min) * unit + midi_min)
+def unit_to_hz2(unit, hz_min: float, hz_max: float) :
+    """Map unit interval [0, 1] to [hz_min, hz_max], scaling logarithmically.
+    
+    Args:
+        unit :: Tensor
+    """
+    midi_max = _hz_to_midi(hz_max)
+    midi_min = _hz_to_midi(hz_min)
+    # [0, 1] -> [min_midi , max_midi] -> [min_hz, max_hz]
+    return _midi_to_hz(midi_min + (midi_max - midi_min) * unit)
+#### /unit_to_hz2 #####################################################################################
 
 
-def get_fft_size(frame_size: int, ir_size: int, power_of_2: bool = True) -> int:
+#### Common functions #################################################################################
+def upsample(signal, factor):
+    signal = signal.permute(0, 2, 1)
+    signal = nn.functional.interpolate(signal, size=signal.shape[-1] * factor)
+    return signal.permute(0, 2, 1)
+
+
+def remove_above_nyquist(amplitudes, pitch, sampling_rate):
+    """Remove partials above nyquist frequency.
+
+    Args:
+        amplitudes    :: (B, T, Partial) - Partial's amplitude contour
+        pitch         :: (B, T, Partial) - f0 contour [Hz]
+        sampling_rate :: int             - Sampling rate (not nyquist frequency)
+    Returns:
+        amplitudes    :: (B, T, Partial) - Masked amplitude
+    """
+    n_harm = amplitudes.shape[-1]
+    # Partial's frequency :: (Partial,) - k*fo [hz]
+    pitches = pitch * torch.arange(1, n_harm + 1).to(pitch)
+    # zero-mask above nyqust frequency
+    aa = (pitches < sampling_rate / 2).float() + 1e-7
+    return amplitudes * aa
+
+
+def scale_function(x):
+    return 2 * torch.sigmoid(x)**(np.log(10)) + 1e-7
+#### /Common functions ################################################################################
+
+
+#### for WavetableSynthesizer #########################################################################
+def linear_lookup(index, wavetable):
+    '''
+    Args:
+        index: B x T, value: [0, 1]
+        wavetables: B x num_wavetables x len_wavetable
+    Returns:
+        singal: B x num_wavetables x T
+    '''
+    wavetable = torch.cat([wavetable, wavetable[..., 0:1]], dim=-1)
+    wavetable = wavetable.unsqueeze(-1)        # B x C x L x 1
+    
+    index = index.unsqueeze(-1) * 2 - 1        # [0, 1] -> [-1, 1]
+    index_ = torch.zeros_like(index) 
+    index = torch.cat([index_, index], dim=-1) # B x L x 2
+    index = index.unsqueeze(2)                 # B x L x 1 x 2
+
+    signal = F.grid_sample(wavetable, grid=index, mode='bilinear', align_corners=True).squeeze(-1)
+    return signal
+#### /for WavetableSynthesizer ########################################################################
+
+
+#### frequency_filter #################################################################################
+def _get_fft_size(frame_size: int, ir_size: int, power_of_2: bool = True) -> int:
   """Calculate final size for efficient FFT.
   Args:
     frame_size: Size of the audio frame.
@@ -59,68 +114,38 @@ def get_fft_size(frame_size: int, ir_size: int, power_of_2: bool = True) -> int:
   return fft_size
 
 
-def upsample(signal, factor):
-    signal = signal.permute(0, 2, 1)
-    signal = nn.functional.interpolate(signal, size=signal.shape[-1] * factor)
-    return signal.permute(0, 2, 1)
-
-
-def remove_above_nyquist(amplitudes, pitch, sampling_rate):
-    n_harm = amplitudes.shape[-1]
-    pitches = pitch * torch.arange(1, n_harm + 1).to(pitch)
-    aa = (pitches < sampling_rate / 2).float() + 1e-7
-    return amplitudes * aa
-
-
-def scale_function(x):
-    return 2 * torch.sigmoid(x)**(np.log(10)) + 1e-7
-    
-    
-def fft_convolve(audio,
+def _fft_convolve(audio,
                  impulse_response,
                  padding = 'same',
                  delay_compensation = -1,
                  mel_scale_noise = False):
     """Filter audio with frames of time-varying impulse responses.
-    Time-varying filter. Given audio [batch, n_samples], and a series of impulse
-    responses [batch, n_frames, n_impulse_response], splits the audio into frames,
-    applies filters, and then overlap-and-adds audio back together.
-    Applies non-windowed non-overlapping STFT/ISTFT to efficiently compute
-    convolution for large impulse response sizes.
+    Given audio (B, n_samples), and a series of impulse responses (B, n_frames, n_impulse_response]), execute:
+        1. splits the audio into frames
+        2. applies filters
+        3. overlap-and-adds audio back together
+    Applies non-windowed non-overlapping STFT/ISTFT to efficiently compute convolution for large impulse response sizes.
     Args:
-        audio: Input audio. Tensor of shape [batch, audio_timesteps].
-        impulse_response: Finite impulse response to convolve. Can either be a 2-D
-        Tensor of shape [batch, ir_size], or a 3-D Tensor of shape [batch,
-        ir_frames, ir_size]. A 2-D tensor will apply a single linear
-        time-invariant filter to the audio. A 3-D Tensor will apply a linear
-        time-varying filter. Automatically chops the audio into equally shaped
-        blocks to match ir_frames.
-        padding: Either 'valid' or 'same'. For 'same' the final output to be the
-        same size as the input audio (audio_timesteps). For 'valid' the audio is
-        extended to include the tail of the impulse response (audio_timesteps +
-        ir_timesteps - 1).
-        delay_compensation: Samples to crop from start of output audio to compensate
-        for group delay of the impulse response. If delay_compensation is less
-        than 0 it defaults to automatically calculating a constant group delay of
-        the windowed linear phase filter from frequency_impulse_response().
+        audio            :: (B, T)                                 - Input audio
+        impulse_response :: (B, ir_size) | (B, ir_frames, ir_size) - The FIR, 2D for LTI, 3D for LTV
+            Automatically chops the audio into equally shaped blocks to match ir_frames.
+        padding          :: 'valid' | 'same'                       -
+            For 'same' the final output to be the same size as the input audio (T)
+            For 'valid' the audio is extended to include the tail of the impulse response (T + ir_timesteps - 1).
+        delay_compensation                                         - Samples to crop from start of output audio to compensate for group delay of the impulse response.
+            If delay_compensation is less than 0 it defaults to automatically calculating a constant group delay of the windowed linear phase filter from frequency_impulse_response().
     Returns:
-        audio_out: Convolved audio. Tensor of shape
-            [batch, audio_timesteps + ir_timesteps - 1] ('valid' padding) or shape
-            [batch, audio_timesteps] ('same' padding).
+        :: (B, T + ir_timesteps - 1) | (B, T) - Filtered audio by 'valid' padding | 'same' padding
     Raises:
         ValueError: If audio and impulse response have different batch size.
         ValueError: If audio cannot be split into evenly spaced frames. (i.e. the
         number of impulse response frames is on the order of the audio size and
         not a multiple of the audio size.)
     """
-    #audio, impulse_response = tf_float32(audio), tf_float32(impulse_response)
-
-    # Add a frame dimension to impulse response if it doesn't have one.
-    #ir_shape = impulse_response.shape.as_list()
+    # LTI-to-psuedoLTV :: (B, ir_size) | (B, ir_frames, ir_size) -> (B, ir_frames=1|f, ir_size)
     ir_shape = impulse_response.size()
     if len(ir_shape) == 2:
         impulse_response = impulse_response.unsqueeze(1)
-        #ir_shape = impulse_response.shape.as_list()
         ir_shape = impulse_response.size()
 
     # Get shapes of audio and impulse response.
@@ -156,16 +181,14 @@ def fft_convolve(audio,
             'size.'.format(n_audio_frames, n_ir_frames))
     
     # Pad and FFT the audio and impulse responses.
-    fft_size = get_fft_size(frame_size, ir_size, power_of_2=True)
-    
-    audio_fft = torch.fft.rfft(audio_frames, fft_size)
+    fft_size = _get_fft_size(frame_size, ir_size, power_of_2=True)
 
-    ir_fft = torch.fft.rfft(impulse_response, fft_size)
-    # Multiply the FFTs (same as convolution in time).
+    # Convolution through fourier-space    
+    audio_fft = torch.fft.rfft(audio_frames,     fft_size)
+    ir_fft    = torch.fft.rfft(impulse_response, fft_size)
     audio_ir_fft = torch.multiply(audio_fft, ir_fft)
-
-    # Take the IFFT to resynthesize audio.
     audio_frames_out = torch.fft.irfft(audio_ir_fft)
+
     #audio_out = torch.signal.overlap_and_add(audio_frames_out, hop_size)
     if frame_size!=audio_size:
         overlap_add_filter = torch.eye(audio_frames_out.size(-1), requires_grad = False).unsqueeze(1).cuda()
@@ -182,21 +205,18 @@ def fft_convolve(audio,
     # Crop and shift the output audio.
 
 
-def apply_window_to_impulse_response(impulse_response,
+def _apply_window_to_impulse_response(impulse_response,
                                      window_size: int = 0,
                                      causal: bool = False):
     """Apply a window to an impulse response and put in causal form.
+
     Args:
-        impulse_response: A series of impulse responses frames to window, of shape
-        [batch, n_frames, ir_size]. ---------> ir_size means size of filter_bank ??????
-        
-        window_size: Size of the window to apply in the time domain. If window_size
-        is less than 1, it defaults to the impulse_response size.
+        impulse_response: A series of impulse responses frames to window, of shape [batch, n_frames, ir_size]. ---------> ir_size means size of filter_bank ??????
+        window_size: Size of the window to apply in the time domain.
+            If window_size is less than 1, it defaults to the impulse_response size.
         causal: Impulse response input is in causal form (peak in the middle).
     Returns:
-        impulse_response: Windowed impulse response in causal form, with last
-        dimension cropped to window_size if window_size is greater than 0 and less
-        than ir_size.
+        impulse_response: Windowed impulse response in causal form, with last dimension cropped to window_size if window_size is greater than 0 and less than ir_size.
     """
     
     
@@ -206,7 +226,6 @@ def apply_window_to_impulse_response(impulse_response,
     
     # Get a window for better time/frequency resolution than rectangular.
     # Window defaults to IR size, cannot be bigger.
-    #ir_size = int(impulse_response.shape[-1])
     ir_size = int(impulse_response.size(-1))
     if (window_size <= 0) or (window_size > ir_size):
         window_size = ir_size
@@ -223,7 +242,6 @@ def apply_window_to_impulse_response(impulse_response,
         window = window.roll((window.size(-1)+1)//2, -1)
         
     # Apply the window, to get new IR (both in zero-phase form).
-
     window = window.unsqueeze(0)
     impulse_response = impulse_response*window
     
@@ -231,30 +249,29 @@ def apply_window_to_impulse_response(impulse_response,
     if padding > 0:
         first_half_start = (ir_size - (half_idx - 1)) + 1
         second_half_end = half_idx + 1
-        impulse_response = torch.cat([impulse_response[..., first_half_start:],
-                                    impulse_response[..., :second_half_end]],
-                                    dim=-1)
+        impulse_response = torch.cat([
+                impulse_response[..., first_half_start:               ],
+                impulse_response[...,                 :second_half_end],
+            ], dim=-1)
     else:
         impulse_response = impulse_response.roll((impulse_response.size(-1)+1)//2, -1)
 
     return impulse_response
 
-def frequency_impulse_response(magnitudes,
-                               window_size: int = 0):
+
+def _frequency_impulse_response(magnitudes, window_size: int = 0):
     """Get windowed impulse responses using the frequency sampling method.
-    Follows the approach in:
-    https://ccrma.stanford.edu/~jos/sasp/Windowing_Desired_Impulse_Response.html
+    Follows the approach in: https://ccrma.stanford.edu/~jos/sasp/Windowing_Desired_Impulse_Response.html
+
     Args:
-        magnitudes: Frequency transfer curve. Float32 Tensor of shape [batch,
-        n_frames, n_frequencies] or [batch, n_frequencies]. The frequencies of the
-        last dimension are ordered as [0, f_nyqist / (n_frequencies -1), ...,
-        f_nyquist], where f_nyquist is (sample_rate / 2). Automatically splits the
-        audio into equally sized frames to match frames in magnitudes.
-        window_size: Size of the window to apply in the time domain. If window_size
-        is less than 1, it defaults to the impulse_response size.
+        magnitudes :: (B, n_frames, n_frequencies) | (B, n_frequencies) - Frequency transfer curve. 
+            The frequencies of the last dimension are ordered as [0, f_nyqist / (n_frequencies -1), ..., f_nyquist] (`f_nyquist=sr/2`).
+            Automatically splits the audio into equally sized frames to match frames in magnitudes.
+        window_size                                                     - Size of the window to apply in the time domain.
+            If window_size is less than 1, it defaults to the impulse_response size.
     Returns:
-        impulse_response: Time-domain FIR filter of shape
-        [batch, frames, window_size] or [batch, window_size].
+        impulse_response :: (B, frames, window_size) | (B, window_size) - Time-domain FIR filter
+        
     Raises:
         ValueError: If window size is larger than fft size.
     """
@@ -263,16 +280,13 @@ def frequency_impulse_response(magnitudes,
     magnitudes = torch.complex(magnitudes, torch.zeros_like(magnitudes))
     impulse_response = torch.fft.irfft(magnitudes)
     
-    #print ("impulse response size here:", impulse_response[0,0, :5], impulse_response[0,0, -5:])
-
     """ This means this?
     First: Initilize at fourier space, where real part equal magnitueds, complex part equal 0
     Second: Convert back to time domain
     """ 
     
     # Window and put in causal form.
-    impulse_response = apply_window_to_impulse_response(impulse_response,
-                                                        impulse_response.size(-1))
+    impulse_response = _apply_window_to_impulse_response(impulse_response, impulse_response.size(-1))
     return impulse_response
 
 
@@ -302,31 +316,6 @@ def frequency_filter(audio,
             [batch, audio_timesteps] ('same' padding).
     """
 
-    impulse_response = frequency_impulse_response(magnitudes,
-                                                    window_size=window_size)
-    
-    return fft_convolve(audio, impulse_response, padding=padding, mel_scale_noise=mel_scale_noise)
-
-
-def linear_lookup(index, wavetable):
-    '''
-    Args:
-        index: B x T, value: [0, 1]
-        wavetables: B x num_wavetables x len_wavetable
-    Returns:
-        singal: B x num_wavetables x T
-    '''
-    wavetable = torch.cat([wavetable, wavetable[..., 0:1]], dim=-1)
-    wavetable = wavetable.unsqueeze(-1)        # B x C x L x 1
-    
-    index = index.unsqueeze(-1) * 2 - 1        # [0, 1] -> [-1, 1]
-    index_ = torch.zeros_like(index) 
-    index = torch.cat([index_, index], dim=-1) # B x L x 2
-    index = index.unsqueeze(2)                 # B x L x 1 x 2
-
-    signal = F.grid_sample(
-        wavetable, 
-        grid=index, 
-        mode='bilinear', 
-        align_corners=True).squeeze(-1)
-    return signal
+    impulse_response = _frequency_impulse_response(magnitudes, window_size=window_size)
+    return _fft_convolve(audio, impulse_response, padding=padding, mel_scale_noise=mel_scale_noise)
+#### /frequency_filter ################################################################################
