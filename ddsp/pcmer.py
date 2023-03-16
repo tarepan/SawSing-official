99e58295f78f1f7937a0607c689aeca0fc2540ce
tarepan <tarepan5884@gmail.com>
2023-03-16 06:37:06 +0000

    Refactor conformer

--- ddsp/pcmer.py
+++ ddsp/pcmer.py
@@ -9,6 +9,7 @@ from local_attention import LocalAttention
 import torch.nn.functional as F
 import fast_transformers.causal_product.causal_product_cuda
 
+
 def softmax_kernel(data, *, projection_matrix, is_query, normalize_data=True, eps=1e-4, device = None):
     b, h, *_ = data.shape
     # (batch size, head, length, model_dim)
@@ -44,7 +45,7 @@ def softmax_kernel(data, *, projection_matrix, is_query, normalize_data=True, ep
 
     return data_dash.type_as(data)
 
-def orthogonal_matrix_chunk(cols, qr_uniform_q = False, device = None):
+def _orthogonal_matrix_chunk(cols, qr_uniform_q = False, device = None):
     unstructured_block = torch.randn((cols, cols), device = device)
     q, r = torch.qr(unstructured_block.cpu(), some = True)
     q, r = map(lambda t: t.to(device), (q, r))
@@ -55,6 +56,7 @@ def orthogonal_matrix_chunk(cols, qr_uniform_q = False, device = None):
         d = torch.diag(r, 0)
         q *= d.sign()
     return q.t()
+
 def exists(val):
     return val is not None
 
@@ -64,49 +66,31 @@ def empty(tensor):
 def default(val, d):
     return val if exists(val) else d
 
-def cast_tuple(val):
-    return (val,) if not isinstance(val, tuple) else val
 
 class PCmer(nn.Module):
     """The encoder that is used in the Transformer model."""
     
-    def __init__(self, 
-                num_layers,
-                num_heads,
-                dim_model,
-                dim_keys,
-                dim_values,
-                residual_dropout,
-                attention_dropout):
+    def __init__(self, num_layers, num_heads, dim_model, dim_keys, dim_values, residual_dropout, attention_dropout):
+        """
+        Args:
+            num_layers - The number of layers
+            num_heads  - The number of attention heads
+        """
         super().__init__()
-        self.num_layers = num_layers
-        self.num_heads = num_heads
-        self.dim_model = dim_model
-        self.dim_values = dim_values
-        self.dim_keys = dim_keys
-        self.residual_dropout = residual_dropout
-        self.attention_dropout = attention_dropout
-
+        # Accessed from childlen instances
+        self.num_layers, self.num_heads, self.dim_model, self.dim_values, self.dim_keys = num_layers, num_heads, dim_model, dim_values, dim_keys
+        self.residual_dropout, self.attention_dropout = residual_dropout, attention_dropout
+        # Use `self` as an augment representing parent (whole conformer)
         self._layers = nn.ModuleList([_EncoderLayer(self) for _ in range(num_layers)])
         
-    #  METHODS  ########################################################################################################
-    
     def forward(self, phone, mask=None):
-        
-        # apply all layers to the input
-        for (i, layer) in enumerate(self._layers):
+        for layer in self._layers:
             phone = layer(phone, mask)
-        # provide the final sequence
         return phone
 
 
-# ==================================================================================================================== #
-#  CLASS  _ E N C O D E R  L A Y E R                                                                                   #
-# ==================================================================================================================== #
-
-
 class _EncoderLayer(nn.Module):
-    """One layer of the encoder.
+    """Encoder layer of .
     
     Attributes:
         attn: (:class:`mha.MultiHeadAttention`): The attention mechanism that is used to read the input sequence.
@@ -114,45 +98,34 @@ class _EncoderLayer(nn.Module):
     """
     
     def __init__(self, parent: PCmer):
-        """Creates a new instance of ``_EncoderLayer``.
-        
+        """        
         Args:
             parent (Encoder): The encoder that the layers is created for.
         """
         super().__init__()
-        
-        
-        self.conformer = ConformerConvModule(parent.dim_model)
-        self.norm = nn.LayerNorm(parent.dim_model)
-        self.dropout = nn.Dropout(parent.residual_dropout)
-        
-        # selfatt -> fastatt: performer!
-        self.attn = SelfAttention(dim = parent.dim_model,
-                                  heads = parent.num_heads,
-                                  causal = False)
-        
-    #  METHODS  ########################################################################################################
 
+        # NOTE: selfatt -> fastatt: performer!
+        self.norm      = nn.LayerNorm(parent.dim_model)
+        self.attn      = SelfAttention(dim = parent.dim_model, heads = parent.num_heads, causal = False)
+        self.conformer = ConformerConvModule(parent.dim_model)
+        
     def forward(self, phone, mask=None):
-        
-        # compute attention sub-layer
-        phone = phone + (self.attn(self.norm(phone), mask=mask))
-        
-        phone = phone + (self.conformer(phone))
-        
+        phone = phone + self.attn(self.norm(phone), mask=mask)
+        phone = phone + self.conformer(phone)
         return phone 
 
-def calc_same_padding(kernel_size):
+
+
+#### ConformerConvModule #############################################################
+def _calc_same_padding(kernel_size):
     pad = kernel_size // 2
     return (pad, pad - (kernel_size + 1) % 2)
 
-# helper classes
-
-class Swish(nn.Module):
+class _Swish(nn.Module):
     def forward(self, x):
         return x * x.sigmoid()
 
-class Transpose(nn.Module):
+class _Transpose(nn.Module):
     def __init__(self, dims):
         super().__init__()
         assert len(dims) == 2, 'dims must be a tuple of two dimensions'
@@ -161,7 +134,8 @@ class Transpose(nn.Module):
     def forward(self, x):
         return x.transpose(*self.dims)
 
-class GLU(nn.Module):
+class _GLU(nn.Module):
+    "Gated Linear Unit"
     def __init__(self, dim):
         super().__init__()
         self.dim = dim
@@ -170,7 +144,7 @@ class GLU(nn.Module):
         out, gate = x.chunk(2, dim=self.dim)
         return out * gate.sigmoid()
 
-class DepthWiseConv1d(nn.Module):
+class _DepthWiseConv1d(nn.Module):
     def __init__(self, chan_in, chan_out, kernel_size, padding):
         super().__init__()
         self.padding = padding
@@ -180,36 +154,43 @@ class DepthWiseConv1d(nn.Module):
         x = F.pad(x, self.padding)
         return self.conv(x)
 
+
 class ConformerConvModule(nn.Module):
-    def __init__(
-        self,
-        dim,
-        causal = False,
-        expansion_factor = 2,
-        kernel_size = 31,
-        dropout = 0.):
+    """
+    Model: LN-Conv-GLU-Conv-Swish-Conv-DO
+           = LN-PointwiseConv1d-GLU-DepthWiseConv1d-Swish-PointwiseConv1d-DO
+    """
+    def __init__(self, dim: int, causal: bool = False, expansion_factor: int = 2, kernel_size: int = 31, dropout: float = 0.):
+        """
+        Args:
+            dim              - Feature dimension size of I/O
+            expansion_factor - Feature dimension expansion factor of middle layer
+        """
         super().__init__()
 
         inner_dim = dim * expansion_factor
-        padding = calc_same_padding(kernel_size) if not causal else (kernel_size - 1, 0)
+        padding = _calc_same_padding(kernel_size) if not causal else (kernel_size - 1, 0)
 
         self.net = nn.Sequential(
             nn.LayerNorm(dim),
-            Transpose((1, 2)),
+            _Transpose((1, 2)),
             nn.Conv1d(dim, inner_dim * 2, 1),
-            GLU(dim=1),
-            DepthWiseConv1d(inner_dim, inner_dim, kernel_size = kernel_size, padding = padding),
+            _GLU(dim=1),
+            _DepthWiseConv1d(inner_dim, inner_dim, kernel_size = kernel_size, padding = padding),
             #nn.BatchNorm1d(inner_dim) if not causal else nn.Identity(),
-            Swish(),
+            _Swish(),
             nn.Conv1d(inner_dim, dim, 1),
-            Transpose((1, 2)),
+            _Transpose((1, 2)),
             nn.Dropout(dropout)
         )
 
     def forward(self, x):
         return self.net(x)
+#### /ConformerConvModule ############################################################
 
-def linear_attention(q, k, v):
+
+#### Attention #######################################################################
+def _linear_attention(q, k, v):
     if v is None:
         #print (k.size(), q.size())
         out = torch.einsum('...ed,...nd->...ne', k, q)
@@ -225,13 +206,13 @@ def linear_attention(q, k, v):
         out = torch.einsum('...de,...nd,...n->...ne', context, q, D_inv)
         return out
 
-def gaussian_orthogonal_random_matrix(nb_rows, nb_columns, scaling = 0, qr_uniform_q = False, device = None):
+def _gaussian_orthogonal_random_matrix(nb_rows, nb_columns, scaling = 0, qr_uniform_q = False, device = None):
     nb_full_blocks = int(nb_rows / nb_columns)
     #print (nb_full_blocks)
     block_list = []
 
     for _ in range(nb_full_blocks):
-        q = orthogonal_matrix_chunk(nb_columns, qr_uniform_q = qr_uniform_q, device = device)
+        q = _orthogonal_matrix_chunk(nb_columns, qr_uniform_q = qr_uniform_q, device = device)
         block_list.append(q)
     # block_list[n] is a orthogonal matrix ... (model_dim * model_dim)
     #print (block_list[0].size(), torch.einsum('...nd,...nd->...n', block_list[0], torch.roll(block_list[0],1,1)))
@@ -239,7 +220,7 @@ def gaussian_orthogonal_random_matrix(nb_rows, nb_columns, scaling = 0, qr_unifo
     remaining_rows = nb_rows - nb_full_blocks * nb_columns
     #print (remaining_rows)
     if remaining_rows > 0:
-        q = orthogonal_matrix_chunk(nb_columns, qr_uniform_q = qr_uniform_q, device = device)
+        q = _orthogonal_matrix_chunk(nb_columns, qr_uniform_q = qr_uniform_q, device = device)
         #print (q[:remaining_rows].size())
         block_list.append(q[:remaining_rows])
 
@@ -254,6 +235,7 @@ def gaussian_orthogonal_random_matrix(nb_rows, nb_columns, scaling = 0, qr_unifo
 
     return torch.diag(multiplier) @ final_matrix
 
+
 class FastAttention(nn.Module):
     def __init__(self, dim_heads, nb_features = None, ortho_scaling = 0, causal = False, generalized_attention = False, kernel_fn = nn.ReLU(), qr_uniform_q = False, no_projection = False):
         super().__init__()
@@ -263,7 +245,7 @@ class FastAttention(nn.Module):
         self.nb_features = nb_features
         self.ortho_scaling = ortho_scaling
 
-        self.create_projection = partial(gaussian_orthogonal_random_matrix, nb_rows = self.nb_features, nb_columns = dim_heads, scaling = ortho_scaling, qr_uniform_q = qr_uniform_q)
+        self.create_projection = partial(_gaussian_orthogonal_random_matrix, nb_rows = self.nb_features, nb_columns = dim_heads, scaling = ortho_scaling, qr_uniform_q = qr_uniform_q)
         projection_matrix = self.create_projection()
         self.register_buffer('projection_matrix', projection_matrix)
 
@@ -305,15 +287,31 @@ class FastAttention(nn.Module):
             q = create_kernel(q, is_query = True)
             k = create_kernel(k, is_query = False)
 
-        attn_fn = linear_attention if not self.causal else self.causal_linear_fn
+        attn_fn = _linear_attention if not self.causal else self.causal_linear_fn
         if v is None:
             out = attn_fn(q, k, None)
             return out
         else:
             out = attn_fn(q, k, v)
             return out
+
+
 class SelfAttention(nn.Module):
-    def __init__(self, dim, causal = False, heads = 8, dim_head = 64, local_heads = 0, local_window_size = 256, nb_features = None, feature_redraw_interval = 1000, generalized_attention = False, kernel_fn = nn.ReLU(), qr_uniform_q = False, dropout = 0., no_projection = False):
+    def __init__(self,
+        dim,
+        causal = False,
+        heads = 8,
+        dim_head = 64,
+        local_heads = 0,
+        local_window_size = 256,
+        nb_features = None,
+        feature_redraw_interval = 1000,
+        generalized_attention = False,
+        kernel_fn = nn.ReLU(),
+        qr_uniform_q = False,
+        dropout = 0.,
+        no_projection = False
+    ):
         super().__init__()
         assert dim % heads == 0, 'dimension must be divisible by number of heads'
         dim_head = default(dim_head, dim // heads)
@@ -378,3 +376,4 @@ class SelfAttention(nn.Module):
         out = rearrange(out, 'b h n d -> b n (h d)')
         out =  self.to_out(out)
         return self.dropout(out)
+#### /Attention ######################################################################
